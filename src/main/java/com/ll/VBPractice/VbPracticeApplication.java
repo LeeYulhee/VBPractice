7d0fa399eabb50f35c1c5b63faa2fa37f4f264b8
LeeYulhee <reasonoflife39@gmail.com>
2023-04-24 13:45:11 +0900

    세팅

---
+++ src/main/java/com/ll/VBPractice/VbPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.ll.VBPractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VbPracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VbPracticeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ll/VBPractice/VbPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ll.VBPractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VbPracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
