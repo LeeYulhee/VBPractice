f714e33296bf2760b9d8ff1f865ac40611d2346f
LeeYulhee <reasonoflife39@gmail.com>
2023-04-24 16:14:43 +0900

    세팅

--- src/main/java/com/ll/VBPractice/HomeController.java
+++ src/main/java/com/ll/VBPractice/HomeController.java
@@ -5,11 +5,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
-public class MainController {
+public class HomeController {
 
     @GetMapping("/")
     @ResponseBody
-    public String home() {
-        return "Hello World";
+    public String showMain() {
+        return "<h1>Hello World</h1>";
     }
 }
